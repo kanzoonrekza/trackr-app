f1e50f88bee35e21a5fb1f1cf1591cb36ba4cca6
kanzoonrekza <kanzoonrkz@gmail.com>
2024-07-11 07:36:06 +0700

    fix env setup and example hello in index

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -6,21 +6,19 @@ export const Route = createLazyFileRoute("/")({
 });
 
 function Index() {
-	const { isPending, data } = useQuery({
+	const { data, isLoading } = useQuery({
 		queryKey: ["todos"],
-		queryFn: () =>
-			new Promise<string>((resolve) => {
-				setTimeout(() => {
-					resolve("Mock fetching data");
-				}, 1000);
-			}),
+		queryFn: async () => {
+			const response = await fetch(`${import.meta.env.VITE_BASE_URL}/hello`);
+			return response.json();
+		},
 	});
 
 	return (
 		<div className="p-2">
 			<h3>Welcome Home!</h3>
 			<Link to="/about">About</Link>
-			<p>{isPending ? "Loading..." : data}</p>
+			<p>{isLoading ? "Loading..." : data.message}</p>
 		</div>
 	);
 }
