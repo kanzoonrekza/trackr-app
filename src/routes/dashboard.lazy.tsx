0098a6e498687d513c076fffe48d3dc35f71a54a
kanzoonrekza <kanzoonrkz@gmail.com>
2024-07-11 08:10:02 +0700

    format

--- src/routes/dashboard.lazy.tsx
+++ src/routes/dashboard.lazy.tsx
@@ -9,25 +9,26 @@ export const Route = createLazyFileRoute("/dashboard")({
 function Dashboard() {
 	return (
 		<MainLayout>
-			<div className="mt-10 p-12 mx-auto max-w-screen-xl">
-				<div className="mb-10 flex justify-between">
-					<h3 className="text-4xl font-bold">
-						Trackr List
-					</h3>
-					<Link className="border border-black px-4 py-2 font-medium" to="/createtrackr">
+			<div className="max-w-screen-xl p-12 mx-auto mt-10">
+				<div className="flex justify-between mb-10">
+					<h3 className="text-4xl font-bold">Trackr List</h3>
+					<Link
+						className="px-4 py-2 font-medium border border-black"
+						to="/createtrackr"
+					>
 						New Trackr
 					</Link>
 				</div>
 				<div className="space-y-4">
-					<div className="border border-black p-4 flex justify-between items-center">
+					<div className="flex items-center justify-between p-4 border border-black">
 						<div>
 							<h2 className="text-xl font-semibold">Series title</h2>
 							<p>Episodes</p>
 						</div>
 						<div className="flex items-center space-x-2">
-							<button className="border border-black px-2">+1</button>
-							<button className="border border-black px-2">++</button>
-							<button className="border border-black px-2">...</button>
+							<button className="px-2 border border-black">+1</button>
+							<button className="px-2 border border-black">++</button>
+							<button className="px-2 border border-black">...</button>
 						</div>
 					</div>
 				</div>
