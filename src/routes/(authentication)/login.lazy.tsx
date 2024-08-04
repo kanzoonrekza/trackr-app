3acf41379b8d6ee788b81d64d6c4f3a8cc35580b
kanzoonrekza <kanzoonrkz@gmail.com>
2024-08-04 09:59:45 +0700

    create auth layout, applied to login page

--- src/routes/(authentication)/login.lazy.tsx
+++ src/routes/(authentication)/login.lazy.tsx
@@ -1,49 +1,49 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { FormEvent } from "react";
-import LoginLayout from "../../layout/login-layout";
+import AuthLayout from "../../layout/auth-layout";
 
 export const Route = createLazyFileRoute("/(authentication)/login")({
   component: Login,
 });
 
 export default function Login() {
+  const navigate = useNavigate();
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("Login");
+    navigate({ to: "/dashboard" });
   };
 
   return (
-    <LoginLayout>
-      <div className="flex min-h-screen items-center justify-center bg-white">
-        <div className="w-full max-w-md bg-white">
-          <h2 className="text-center text-3xl font-semibold">Log In</h2>
-          <p className="mt-6 text-center">
-            Continue where you left off and stay on trackr
-          </p>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3 mt-4">
-              <input
-                type="email"
-                placeholder="Email / Username"
-                className="w-full border border-black p-2"
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Password"
-                className="w-full border border-black p-2"
-              />
-            </div>
-            <button
-              type="submit"
-              className="mt-3 w-full bg-black p-2 text-white"
-            >
-              Log In
-            </button>
-          </form>
+    <AuthLayout>
+      <AuthLayout.Title>Log In</AuthLayout.Title>
+      <AuthLayout.SubTitle>
+        Continue where you left off and stay on trackr
+      </AuthLayout.SubTitle>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+        <div>
+          <input
+            type="email"
+            placeholder="Email / Username"
+            className="w-full border border-black p-2"
+          />
         </div>
-      </div>
-    </LoginLayout>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full border border-black p-2"
+          />
+        </div>
+        <button type="submit" className="w-full bg-black p-2 text-white">
+          Log In
+        </button>
+      </form>
+      <AuthLayout.Redirect>
+        Don't have an account?&nbsp;
+        <Link className="underline" to="/signup">
+          Sign Up
+        </Link>
+      </AuthLayout.Redirect>
+    </AuthLayout>
   );
 }
