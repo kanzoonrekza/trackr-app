6f6a2f4dd7b43ebdd3715060d8b79f4a591a62bb
kanzoonrekza <kanzoonrkz@gmail.com>
2024-08-05 10:13:07 +0700

    add some fields for myanimelist search result

--- src/services/external/myAnimeList.ts
+++ src/services/external/myAnimeList.ts
@@ -4,7 +4,7 @@ export const FetchMyAnimeList = {
       return;
     }
     const response = await fetch(
-      `${import.meta.env.VITE_MAL_BASE_URL}/${category}?q=${query}&limit=6`,
+      `${import.meta.env.VITE_MAL_BASE_URL}/${category}?q=${query}&limit=6&fields=alternative_titles,status,mean,start_date,end_date,num_episodes,start_season`,
       {
         headers: {
           "X-MAL-CLIENT-ID": import.meta.env.VITE_MAL_CLIENT_ID,
